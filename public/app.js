b96c9a7b2f0f9307891867f2883716de5f2531ea
almed4 <alexandre.meddin@ingka.ikea.com>
2021-03-03 15:14:08 -0500

    add things list and firebase devops support

--- public/app.js
+++ public/app.js
@@ -1,1 +1,61 @@
-console.log(firebase)
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+const whenSignedIn = document.getElementById('whenSignedIn');
+const whenSignedOut = document.getElementById('whenSignedOut');
+
+const signInBtn = document.getElementById('signInBtn');
+const signOutBtn = document.getElementById('signOutBtn');
+
+const userDetails = document.getElementById('userDetails');
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+signInBtn.onclick = () => auth.signInWithPopup(provider);
+signOutBtn.onclick = () => auth.signOut();
+
+auth.onAuthStateChanged(user => {
+    if (user) {
+        whenSignedIn.hidden = false;
+        whenSignedOut.hidden = true;
+        userDetails.innerHTML = `<h3>Hello ${user.displayName}!</h3> <p>User ID: ${user.uid}</p>`;
+    } else {
+        whenSignedIn.hidden = true;
+        whenSignedOut.hidden = false;
+        userDetails.innerHTML = '';
+    }
+});
+
+const createThing = document.getElementById('createThing');
+const thingsList = document.getElementById('thingsList');
+
+let thingsRef;
+let unsubscribe;
+
+auth.onAuthStateChanged(user => {
+    if (user) {
+        thingsRef = db.collection('things');
+
+        createThing.onclick = () => {
+            const {serverTimestamp} = firebase.firestore.FieldValue;
+            thingsRef.add({
+                uid: user.uid,
+                name: faker.commerce.productName(),
+                createdAt: serverTimestamp(),
+            })
+        }
+
+        unsubscribe = thingsRef
+            .where('uid', '==', user.uid)
+            .orderBy('createdAt')
+            .onSnapshot(querySnapshot => {
+                const items = querySnapshot.docs.map(doc => {
+                    return `<li>${doc.data().name}</li>`
+                });
+
+                thingsList.innerHTML = items.join('');
+            });
+    } else {
+        unsubscribe && unsubscribe();
+    }
+});
