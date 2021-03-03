b2675af3b2b3b1d4342b99dc2957881b8ac368c6
ameddin73 <ameddin73@gmail.com>
2021-03-03 18:05:14 -0500

    create react app and switch to firebase app pheedv2

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+
+function App() {
+    return <Button variant="contained" color="primary">Hello World</Button>
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+};
+// Use your config values here.
+firebase.initializeApp(firebaseConfig);
+
+console.log(firebase);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
