8dc4b3ca3222e95fe2dab5e3c48d5387b54772bd
ameddin73 <ameddin73@gmail.com>
2021-03-04 15:43:55 -0500

    setup single page layout

--- src/App.js
+++ src/App.js
@@ -1,36 +1,20 @@
 import React from 'react';
-import {Button, ThemeProvider} from '@material-ui/core';
+import {ThemeProvider} from '@material-ui/core';
 import firebase from "firebase/app";
 import 'firebase/auth';
 import 'firebase/firestore';
-import {FirebaseAuthProvider, IfFirebaseAuthed, IfFirebaseUnAuthed} from "@react-firebase/auth";
+import {FirebaseAuthProvider} from "@react-firebase/auth";
 import {FirestoreProvider} from "@react-firebase/firestore";
-import DistroHub from "./components/DistroHub/DistroHub.lazy";
 import theme from "./theme";
+import Common from "./components/Common/Common.lazy";
 
 function App({config}) {
-    const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
-
     return (
         <div>
             <ThemeProvider theme={theme}>
                 <FirestoreProvider {...config} firebase={firebase}>
                     <FirebaseAuthProvider {...config} firebase={firebase}>
-                        <IfFirebaseUnAuthed>
-                            {({...rest}) => (
-                                <div>
-                                    <Button variant="contained" color="primary" onClick={() => {firebase.auth().signInWithRedirect(googleAuthProvider)}}>Sign In with Google</Button>
-                                </div>
-                            )}
-                        </IfFirebaseUnAuthed>
-                        <IfFirebaseAuthed>
-                            {({user, ...rest}) => (
-                                <div>
-                                    <DistroHub user={user}/>
-                                    <Button variant="contained" onClick={() => firebase.auth().signOut()}>Sign Out</Button>
-                                </div>
-                            )}
-                        </IfFirebaseAuthed>
+                        <Common/>
                     </FirebaseAuthProvider>
                 </FirestoreProvider>
             </ThemeProvider>
