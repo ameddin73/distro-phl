30fb90fbadb926ac9c32bc89d8407d9ba1abf4a9
almed4 <alexandre.meddin@ingka.ikea.com>
2021-03-03 21:41:58 -0500

    implement material and firebase + auth

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
-import React from 'react';
-import { Button } from '@material-ui/core';
+import React, {useState} from 'react';
+import {Button, Typography} from '@material-ui/core';
+import firebase from "firebase/app";
+import 'firebase/auth';
+import {FirebaseAuthProvider, IfFirebaseAuthed, IfFirebaseUnAuthed} from "@react-firebase/auth";
 
-function App() {
-    return <Button variant="contained" color="primary">Hello World</Button>
+function App({config}) {
+    const [user, setUser] = useState({displayName: 'user'});
+    const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+
+    const onSignIn = () => {
+        firebase.auth().signInWithPopup(googleAuthProvider)
+            .then(({user}) => setUser(user))
+    }
+
+    return (
+        <FirebaseAuthProvider {...config} firebase={firebase}>
+            <div>
+                <IfFirebaseUnAuthed>
+                    <Button variant="contained" color="primary" onClick={onSignIn}>Sign In with Google</Button>
+                </IfFirebaseUnAuthed>
+                <IfFirebaseAuthed>
+                    <div>
+                        <Typography variant="h6" gutterBottom>
+                            Welcome, {user.displayName}!
+                        </Typography>
+                        <Button variant="contained" onClick={() => firebase.auth().signOut()}>Sign Out</Button>
+                    </div>
+                </IfFirebaseAuthed>
+            </div>
+        </FirebaseAuthProvider>
+    )
 }
 
 export default App;
