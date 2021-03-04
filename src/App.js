a7512ff75c2c004f7821108cd1ab244c96110666
ameddin73 <ameddin73@gmail.com>
2021-03-04 16:55:27 -0500

    routing

--- src/App.js
+++ src/App.js
@@ -7,14 +7,23 @@ import {FirebaseAuthProvider} from "@react-firebase/auth";
 import {FirestoreProvider} from "@react-firebase/firestore";
 import theme from "./theme";
 import Common from "./components/Common/Common.lazy";
+import DistroHub from "./components/DistroHub/DistroHub.lazy";
+import {useRoutes} from "hookrouter";
+
+const routes = {
+    '/': () => <DistroHub/>,
+}
 
 function App({config}) {
+    const routeResult = useRoutes(routes);
+
     return (
         <div>
             <ThemeProvider theme={theme}>
                 <FirestoreProvider {...config} firebase={firebase}>
                     <FirebaseAuthProvider {...config} firebase={firebase}>
                         <Common/>
+                        {routeResult || routes['/']}
                     </FirebaseAuthProvider>
                 </FirestoreProvider>
             </ThemeProvider>
