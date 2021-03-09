ad78f51b0e1d021fb76121b2ebc2db91823ef512
ameddin73 <ameddin73@gmail.com>
2021-03-09 12:24:15 -0500

    loading position oops

--- src/components/Common/loading.js
+++ src/components/Common/loading.js
@@ -2,18 +2,13 @@ import {CircularProgress} from "@material-ui/core";
 import React from "react";
 import {makeStyles} from "@material-ui/styles";
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        width: '100%',
-        maxWidth: 360,
-        backgroundColor: theme.palette.background.paper,
-        flexGrow: 1,
-    },
+const useStyles = makeStyles({
     loading: {
         position: 'absolute',
+        left: '50%',
         top: '25%',
     },
-}));
+});
 
 export const Loading = () => {
     const classes = useStyles();
