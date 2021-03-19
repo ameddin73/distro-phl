7f329d8b034560e16307ef8cff365b302589a6d8
ameddin73 <ameddin73@gmail.com>
2021-03-19 14:34:51 -0400

    refactor usermenu to use reactfire and also try converting it to a drawer but hard mode since the whole app doesn't compile

--- src/components/Common/TopBar/UserMenu/UserMenu.tsx
+++ src/components/Common/TopBar/UserMenu/UserMenu.tsx
@@ -1,58 +1,63 @@
 import React, {SyntheticEvent, useState} from 'react';
-import {IfFirebaseAuthed, IfFirebaseUnAuthed} from "@react-firebase/auth";
-import {Button, Menu, MenuItem} from "@material-ui/core";
+import {Button, List, ListItem, ListItemText, SwipeableDrawer} from "@material-ui/core";
 import {Menu as MenuIcon} from "@material-ui/icons";
-import firebase from "firebase/app";
 // @ts-ignore
 import {navigate, usePath} from 'hookrouter';
 import {paths} from "../../../../util/config";
+import {AuthCheck, useAuth, useUser} from "reactfire";
 
-const UserMenu = () => {
-    const [anchorEl, setAnchorEl] = useState<Element | null>(null);
+const UMenu = () => {
+    const [open, setOpen] = useState(false);
+    const {data: user} = useUser();
+    const auth = useAuth();
 
-    const path = usePath();
-
-    // const menuClick = (event: Event) => (setAnchorEl(event.currentTarget));
-    const menuClick = (event: SyntheticEvent) => (setAnchorEl(event.currentTarget));
-    const menuClose = () => (setAnchorEl(null))
+    const toggleDrawer = (open: boolean) => (event: SyntheticEvent) => (setOpen(open));
     const closeAndAction = (action: () => void) => {
-        menuClose();
+        toggleDrawer(false);
         action();
     }
 
     return (
         <div>
-            <IfFirebaseUnAuthed>
-                {() => (
-                    <Button color="inherit"
-                            onClick={() => navigate(paths.public.login, {redirect: path})}>Login</Button>
-                )}
-            </IfFirebaseUnAuthed>
-            <IfFirebaseAuthed>
-                {({user}) => (
-                    <div>
-                        <Button aria-controls="simple-menu"
-                                aria-haspopup="true"
-                                onClick={menuClick}
-                                endIcon={<MenuIcon/>}>
-                            {user.displayName}
-                        </Button>
-                        <Menu
-                            id="user-menu"
-                            anchorEl={anchorEl}
-                            keepMounted
-                            open={Boolean(anchorEl)}
-                            onClose={menuClose}>
-                            <MenuItem onClick={() => closeAndAction(() => navigate(paths.public.createItem))}>New Item</MenuItem>
-                            <MenuItem onClick={() => closeAndAction(() => navigate(paths.public.userItems))}>My Items</MenuItem>
-                            <MenuItem onClick={() => closeAndAction(() => {
-                                firebase.auth().signOut();
-                                navigate(paths.public.distro);
-                            })}>Sign Out</MenuItem>
-                        </Menu>
-                    </div>
-                )}
-            </IfFirebaseAuthed>
+            <Button aria-controls="simple-menu"
+                    aria-haspopup="true"
+                    onClick={toggleDrawer(true)}
+                    endIcon={<MenuIcon/>}>
+                {user.displayName}
+            </Button>
+            <SwipeableDrawer onClose={toggleDrawer(false)}
+                             onOpen={toggleDrawer(true)}
+                             open={open}>
+                <List>
+                    <ListItem button key="new-item" onClick={() => closeAndAction(() => navigate(paths.public.createItem))}>
+                        <ListItemText primary="New Item"/>
+                    </ListItem>
+                    <ListItem button key="my-items" onClick={() => closeAndAction(() => navigate(paths.public.userItems))}>
+                        <ListItemText primary="My Items"/>
+                    </ListItem>
+                    <ListItem button key="sign-out" onClick={() => closeAndAction(() => {
+                        auth.signOut().then();
+                        navigate(paths.public.distro);
+                    })}>
+                        <ListItemText primary="Sign Out"/>
+                    </ListItem>
+                </List>
+            </SwipeableDrawer>
+        </div>
+    )
+}
+
+const UserMenu = () => {
+    const path = usePath();
+
+    return (
+        <div>
+            <AuthCheck fallback={
+                <Button color="inherit"
+                        onClick={() => navigate(paths.public.login, {redirect: path})}>Sign In</Button>
+            }>
+                <UMenu/>
+            </AuthCheck>
         </div>
     );
 }
