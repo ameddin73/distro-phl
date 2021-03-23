8fee910cce5aeceb5b38d223d9f392ab4660fb2b
ameddin73 <ameddin73@gmail.com>
2021-03-23 10:26:21 -0400

    tidy up ErrorMessage.tsx

--- src/components/Common/ErrorMessage.tsx
+++ src/components/Common/ErrorMessage.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import {Grid, Typography} from "@material-ui/core";
+import {Button, Grid, Typography} from "@material-ui/core";
 import {makeStyles} from "@material-ui/styles";
 import {FallbackProps} from "react-error-boundary";
+import {useHistory} from "react-router-dom";
 
 const useStyles = makeStyles({
     body: {
@@ -12,15 +13,20 @@ const useStyles = makeStyles({
 
 const ErrorMessage = ({error}: FallbackProps) => {
     const classes = useStyles();
+    const history = useHistory();
 
     return (
-        <div className={classes.body}>
-            <Grid container direction="column" alignItems="center" spacing={2}>
+        <>
+            <Grid container direction="column" alignItems="center" alignContent="center" spacing={2} className={classes.body}>
                 <Grid item xs>
                     <Typography role="alert" variant="h5" color="primary" align="center">{error.message}</Typography>
                 </Grid>
+                <Grid item xs>
+                    <Button variant="contained" color="primary"
+                            onClick={() => history.go(0)}>Try Again</Button>
+                </Grid>
             </Grid>
-        </div>
+        </>
     )
 };
 
