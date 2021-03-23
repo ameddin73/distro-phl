738b3db50c42f012a08bfa867aabd29ef6a57894
ameddin73 <ameddin73@gmail.com>
2021-03-23 16:07:42 -0400

    use types hook for item

--- src/components/Common/Item/Item.tsx
+++ src/components/Common/Item/Item.tsx
@@ -1,21 +1,21 @@
 import React, {useState} from 'react';
 import {Card, CardActionArea, CardContent, ClickAwayListener, Collapse, Grid, Typography} from "@material-ui/core";
-// @ts-ignore
 import {itemStyle} from "../../../util/styles";
-import {ItemInterface, ItemTypes} from '../../../util/types';
+import {ItemInterface} from '../../../util/types';
 import {DEFAULT_IMAGE} from "../../../util/config";
 import {StorageImage} from "reactfire";
 import Loading from "../Loading";
+import {useItemTypes} from "../../../util/hooks";
 
 export type ItemProps = {
     item: ItemInterface,
-    types: ItemTypes,
     itemAction?: (item: ItemInterface) => JSX.Element,
 }
 
-const Item = ({item, types, itemAction = (() => (<div/>))}: ItemProps) => {
+const Item = ({item, itemAction = (() => (<div/>))}: ItemProps) => {
     const classes = itemStyle();
 
+    const types = useItemTypes();
     const {displayName, description, image} = item;
     const [openDesc, setOpenDesc] = useState(false);
 
