ca565a2cd443569e0a100ba3fbe8b1d527c91d42
ameddin73 <ameddin73@gmail.com>
2021-03-30 14:59:43 -0400

    rewrite item test to successfully implement firestore emulator. HUGE!

--- src/test/utils.tsx
+++ src/test/utils.tsx
@@ -5,17 +5,35 @@ import {FIREBASE_CONFIG} from "util/config";
 import theme from "util/theme";
 import {render, RenderOptions} from "@testing-library/react";
 import SnackbarProvider from "../components/Common/SnackbarProvider/SnackbarProvider";
+import firebase from "firebase";
+
+const PROJECT_ID = `${process.env.TEST_PROJECT}`;
+let firebaseApp: firebase.app.App;
+
+export function setupFirebase() {
+    const firebaseConfig = FIREBASE_CONFIG;
+    firebaseConfig.projectId = PROJECT_ID;
+    firebaseApp = firebase.initializeApp(firebaseConfig);
+    firebaseApp.firestore().useEmulator('localhost', 8080);
+}
+
+export async function resetFirebase() {
+    const firestore = firebaseApp.firestore();
+    await firestore.terminate();
+    await firestore.clearPersistence();
+    firebaseApp.firestore().useEmulator('localhost', 8080);
+}
 
 export const customRender = (ui: React.ReactElement, options?: RenderOptions) => render(ui, {wrapper: Providers, ...options});
 
 const Providers = ({children}: PropsWithChildren<any>) => (
     <ThemeProvider theme={theme}>
-        <FirebaseAppProvider firebaseConfig={FIREBASE_CONFIG} suspense>
+        <FirebaseAppProvider firebaseApp={firebaseApp} suspense>
             <SnackbarProvider>
-                <Suspense fallback={<div/>}>
+                <Suspense fallback={<div id="fallback"/>}>
                     {children}
                 </Suspense>
             </SnackbarProvider>
         </FirebaseAppProvider>
     </ThemeProvider>
-)
+);
