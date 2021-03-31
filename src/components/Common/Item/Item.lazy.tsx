a6b338f48d8b64ad704c4a1a2f02da872a3a0d0f
ameddin73 <ameddin73@gmail.com>
2021-03-31 13:25:43 -0400

    expand preset data and change tests for extra list scenarios

--- src/components/Common/Item/Item.lazy.tsx
+++ src/components/Common/Item/Item.lazy.tsx
@@ -2,11 +2,11 @@ import React, {lazy, Suspense} from 'react';
 import {ItemProps} from "./Item";
 import Loading from "../Loading";
 
-const LazyDistroItem = lazy(() => import('./Item'));
+const LazyItem = lazy(() => import('./Item'));
 
 const DistroItem = (props: ItemProps) => (
     <Suspense fallback={<Loading/>}>
-        <LazyDistroItem {...props} />
+        <LazyItem {...props} />
     </Suspense>
 );
 
