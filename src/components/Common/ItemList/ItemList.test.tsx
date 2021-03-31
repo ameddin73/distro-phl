636c3e782a5d062484dc390c8e0712d8f7f81387
ameddin73 <ameddin73@gmail.com>
2021-03-31 13:37:55 -0400

    add tests for the actual *logic* idiot

--- src/components/Common/ItemList/ItemList.test.tsx
+++ src/components/Common/ItemList/ItemList.test.tsx
@@ -7,6 +7,8 @@ import {customRender, resetFirebase, setupFirebase} from "../../../test/utils";
 import {screen, waitFor} from "@testing-library/react";
 import {COLLECTIONS} from "util/config";
 import {Query} from "util/utils";
+import {FirestoreQueryWhere, ItemInterface} from "util/types";
+import {UserMocks} from "../../../test/mocks/user.mock";
 
 const path = COLLECTIONS.items;
 const orderBy = Query.orderByCreated;
@@ -29,4 +31,33 @@ it('renders all items', async () => {
     await waitFor(() => expect(document.querySelector('#loading')).toBeNull())
     const items = screen.getAllByText('Supplied by:');
     expect(items.length).toBe(7);
+});
+
+it('filters items', async () => {
+    const filter = ((item: ItemInterface) => item.uid !== UserMocks.defaultUser.uid);
+    customRender(<ItemList {...props} filter={filter}/>);
+    await waitFor(() => expect(document.querySelector('#loading')).toBeNull())
+    const items = screen.getAllByText('Supplied by:');
+    expect(items.length).toBe(2);
+});
+
+it('renders NothingHere if query returns empty list', async () => {
+    const newWhere: FirestoreQueryWhere = {
+        fieldPath: 'displayName',
+        opStr: '==',
+        value: 'fake-name',
+    }
+    const newQuery = {
+        where: [Query.whereActive, newWhere],
+    };
+    customRender(<ItemList path={path} query={newQuery}/>);
+    await waitFor(() => expect(document.querySelector('#loading')).toBeNull())
+    screen.getByText('Oops, theres nothing here.');
+});
+
+it('renders NothingHere if filter filters all items', async () => {
+    const filter = ((item: ItemInterface) => false);
+    customRender(<ItemList {...props} filter={filter}/>);
+    await waitFor(() => expect(document.querySelector('#loading')).toBeNull())
+    screen.getByText('Oops, theres nothing here.');
 });
