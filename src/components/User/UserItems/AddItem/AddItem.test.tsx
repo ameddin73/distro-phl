5403390ae65be8b83acd5c5c9bc8ba2708bf4e15
ameddin73 <ameddin73@gmail.com>
2021-04-07 14:47:45 -0400

    skeleton additem tests but leaving incomplete because of imminent feature cahnges

--- src/components/User/UserItems/AddItem/AddItem.test.tsx
+++ src/components/User/UserItems/AddItem/AddItem.test.tsx
@@ -1,9 +1,46 @@
+/**
+ * @jest-environment test/jest-env
+ */
 import React from 'react';
-import ReactDOM from 'react-dom';
-// import AddItem from './AddItem';
+import AddItem from './AddItem';
+import {customRender, resetFirebase, setupFirebase, signIn} from "test/utils";
+import {fireEvent, screen, waitFor} from "@testing-library/react";
 
-it('It should mount', () => {
-  const div = document.createElement('div');
-    // ReactDOM.render(<AddItem />, div); TODO testing
-  ReactDOM.unmountComponentAtNode(div);
+// @ts-ignore
+global.File = class MockFile {
+    name: string;
+    constructor(parts: (string | Blob | ArrayBuffer | ArrayBufferView)[], filename: string, properties ?: FilePropertyBag) {
+        this.name = filename;
+    }
+}
+global.URL.createObjectURL = jest.fn();
+
+beforeAll(async () => {
+    await setupFirebase();
+    await signIn();
 });
+beforeEach(async () => {
+    customRender(<AddItem/>);
+    await waitFor(() => expect(document.querySelector('#loading')).toBeNull())
+});
+afterEach(async () => await resetFirebase());
+
+it('should mount', () => {
+});
+
+it('renders all fields', () => {
+    screen.getByText('Post a new item.');
+    screen.getByText('Name');
+    screen.getByText('Type');
+    screen.getByText('Description');
+    screen.getByText('Submit');
+});
+
+it('attaches image', () => {
+    global.URL.createObjectURL = ({name}) => name;
+    fireEvent.click(screen.getByLabelText('upload-image'));
+    fireEvent.change(screen.getByTestId('image-input'), {target: {files: [new File(['parts'], 'filename.jpeg')]}})
+    expect((screen.getByLabelText('uploaded-image') as HTMLImageElement)).toHaveStyle('background-image: url(filename.jpeg)');
+});
+
+// TODO complete test after feature changes
