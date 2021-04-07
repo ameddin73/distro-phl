74e4f2806022a6f2ad7b0e870a58c41f85b68c0d
ameddin73 <ameddin73@gmail.com>
2021-04-07 17:32:09 -0400

    extend timeout on emulator tests

--- src/components/Common/Item/Item.test.tsx
+++ src/components/Common/Item/Item.test.tsx
@@ -2,7 +2,7 @@
  * @jest-environment test/jest-env
  */
 import React from 'react';
-import {screen, waitFor, waitForElementToBeRemoved} from "@testing-library/react";
+import {screen, waitFor} from "@testing-library/react";
 import Item from "./Item";
 import {customRender, resetFirebase, setupFirebase} from "test/utils";
 import {ItemMocks} from "test/mocks/item.mock";
@@ -16,17 +16,16 @@ const TestItemAction = () => (
     <div>{testItemActionText}</div>
 )
 beforeAll(setupFirebase);
+beforeEach(async () => {
+    customRender(<Item item={mockDefaultItem}/>);
+    await waitFor(() => expect(document.querySelector('#loading')).toBeNull(), {timeout: 60000})
+}, 60000);
 afterEach(async () => await resetFirebase());
 
 it('should mount', async () => {
-    customRender(<Item item={mockDefaultItem}/>);
-    expect(document.querySelector('#loading')).toBeInTheDocument();
-    await waitForElementToBeRemoved(document.querySelector('#loading'));
 });
 
 it('renders item details properly', async () => {
-    customRender(<Item item={mockDefaultItem}/>);
-    await waitFor(() => expect(document.querySelector('#loading')).toBeNull())
     screen.getByText(mockDefaultItem.displayName);
     screen.getByText(mockTypes[mockDefaultItem.type].displayName);
     screen.getByText(mockDefaultItem.description);
@@ -35,6 +34,5 @@ it('renders item details properly', async () => {
 
 it('renders item action properly', async () => {
     customRender(<Item item={mockDefaultItem} itemAction={TestItemAction}/>);
-    await waitFor(() => expect(document.querySelector('#loading')).toBeNull())
-    screen.getByText(testItemActionText);
+    await waitFor(() => screen.getByText(testItemActionText));
 });
