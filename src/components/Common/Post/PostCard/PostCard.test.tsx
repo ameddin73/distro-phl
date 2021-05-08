8d90747edd9b3a138276012cddbf3b89412bf38d
ameddin73 <ameddin73@gmail.com>
2021-05-07 23:15:12 -0400

    fix tests

--- src/components/Common/Post/PostCard/PostCard.test.tsx
+++ src/components/Common/Post/PostCard/PostCard.test.tsx
@@ -2,11 +2,10 @@
  * @jest-environment test/jest-env
  */
 import React from 'react';
-import {fireEvent, screen, waitFor} from "@testing-library/react";
+import {screen, waitFor} from "@testing-library/react";
 import {customRender, resetFirebase, setupFirebase, teardownFirebase} from "test/utils";
 import {PostMocks} from "test/mocks/post.mock";
 import PostCard from "./PostCard";
-import {PATHS} from "util/config";
 import * as MockDate from "mockdate";
 import {PostInterface} from "util/types";
 
@@ -28,25 +27,3 @@ it('does not render expires if no expiration', async () => {
     expect(screen.queryByText('Expires')).toBeNull();
     screen.getByText(mockSecondaryPost.userName);
 });
-
-describe('post with expires', () => {
-    beforeEach(async () => {
-        customRender(<PostCard post={mockDefaultPost}/>);
-        await waitFor(() => expect(document.querySelector('#loading')).toBeNull(), {timeout: 60000})
-    }, 60000);
-
-    it('should mount', async () => {
-    });
-
-    it('renders post details properly', () => {
-        screen.getByText(mockDefaultPost.name);
-        // @ts-ignore
-        screen.getByText(mockDefaultPost.expires.toLocaleDateString('en-US', {month: 'long', day: 'numeric'}));
-        screen.getByText(mockDefaultPost.userName);
-    });
-
-    it('navigates to post', () => {
-        fireEvent.click(screen.getByTestId('card-action'));
-        expect(window.location.pathname).toBe(`${PATHS.public.posts}/${mockDefaultPost.id}`);
-    });
-})
