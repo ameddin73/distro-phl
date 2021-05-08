56328d22dab1cb6d2616e5d27079465e0c4c5e39
ameddin73 <ameddin73@gmail.com>
2021-05-07 22:54:36 -0400

    skeleton loading for cards

--- src/components/Common/Post/PostCard/PostCard.tsx
+++ src/components/Common/Post/PostCard/PostCard.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import {CardActionArea, Grid, Typography} from "@material-ui/core";
-import {postCardStyle} from "./styles";
+import {MEDIA_HEIGHT, postCardStyle} from "./styles";
 import {DEFAULT_IMAGE, PATHS} from "util/config";
 import {StorageImage} from "reactfire";
 import {ErrorBoundary} from "react-error-boundary";
 import Loading from "../../Loading/Loading";
 import RouterLink from "../../RouterLink";
 import {PostInterface} from "util/types";
+import {Skeleton} from "@material-ui/lab";
 
 export type PostProps = {
     post: PostInterface
@@ -20,21 +21,22 @@ const PostCard = ({post}: PostProps) => {
 
     const storageParams = {
         storagePath: image,
-        onError: () => {
-        },
+        onError: () => "",
     }
     storageParams.onError = () => storageParams.storagePath = DEFAULT_IMAGE;
 
     return (
         <CardActionArea component={RouterLink} to={`${PATHS.public.posts}/${id}`} className={classes.action} data-testid="card-action">
             <Grid item>
-                <ErrorBoundary fallback={
-                    <StorageImage suspense={true} placeHolder={<Loading/>} storagePath={DEFAULT_IMAGE} className={classes.media} alt={image ? post.name : 'Default Image'}/>
-                }>
-                    <StorageImage suspense={true} placeHolder={<Loading/>} storagePath={image || DEFAULT_IMAGE} className={classes.media} alt={image ? post.name : 'Default Image'}/>
-                </ErrorBoundary>
+                <Suspense fallback={<Skeleton variant="rect" height={MEDIA_HEIGHT} width="1000%" animation="wave"/>}>
+                    <ErrorBoundary fallback={
+                        <StorageImage suspense={true} placeHolder={<Loading/>} storagePath={DEFAULT_IMAGE} className={classes.media} alt={image ? post.name : 'Default Image'}/>
+                    }>
+                        <StorageImage suspense={true} placeHolder={<Loading/>} storagePath={image || DEFAULT_IMAGE} className={classes.media} alt={image ? post.name : 'Default Image'}/>
+                    </ErrorBoundary>
+                </Suspense>
                 <div className={classes.title}>
-                    <Typography variant="h6">
+                    <Typography variant="h6" noWrap>
                         {name}
                     </Typography>
                 </div>
