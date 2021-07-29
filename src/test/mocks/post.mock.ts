6bf0644bbf02e3351efb010d6cebeaa4995f9672
ameddin73 <ameddin73@gmail.com>
2021-07-28 20:22:57 -0400

    remove phone users, add a test for missing username in rules, and move firebase export to its own directory

--- src/test/mocks/post.mock.ts
+++ src/test/mocks/post.mock.ts
@@ -12,7 +12,7 @@ export namespace PostMocks {
         expires: new Date('02 Jan 2070 00:00:00 GMT'),
         image: 'images/posts/a0.jpg',
         uid: UserMocks.defaultUser.uid,
-        userName: UserMocks.defaultUser.name,
+        userName: UserMocks.defaultUser.name || '',
     };
     export const secondaryPost: Post = {
         id: 'secondary_post_id',
@@ -22,7 +22,7 @@ export namespace PostMocks {
         description: 'secondary_post_description',
         hasExpiration: false,
         uid: UserMocks.userTwo.uid,
-        userName: UserMocks.userTwo.name,
+        userName: UserMocks.userTwo.name || '',
     };
     export const tertiaryPost: Post = {
         id: 'tertiary_post_id',
@@ -33,6 +33,15 @@ export namespace PostMocks {
         hasExpiration: false,
         image: 'images/posts/a1.jpg',
         uid: UserMocks.userThree.uid,
-        userName: UserMocks.userThree.name,
+        userName: UserMocks.userThree.name || '',
+    };
+    export const namelessPost: Post = {
+        id: 'phone_post_id',
+        active: true,
+        created: new Date('01 Jan 1970 00:00:00 GMT'),
+        description: 'phone_post_description',
+        hasExpiration: false,
+        uid: UserMocks.userNameless.uid,
+        userName: 'Distro User',
     };
 }
